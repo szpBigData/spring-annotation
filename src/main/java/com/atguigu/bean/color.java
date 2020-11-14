b0134c31f51d1f9b66ffad3c1c93456472d9f5e5
szp <sunzhipeng96@163.com>
2020-11-14 21:37:06 +0800

    inport注解

---
+++ src/main/java/com/atguigu/bean/color.java
@@ -0,0 +1,8 @@
+package com.atguigu.bean;
+
+/**
+ * @author sunzhipeng
+ * @create 2020-11-14 21:16
+ */
+public class color {
+}
--- src/test/java/com/atguigu/test/IOCTest.java
+++ src/test/java/com/atguigu/test/IOCTest.java
@@ -1,9 +1,16 @@
 package com.atguigu.test;
 
+import com.atguigu.bean.Person;
 import com.atguigu.config.MainConfig;
+import com.atguigu.config.MainConfig2;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+
+import javax.naming.NamingEnumeration;
+import java.util.Map;
 
 
 /**
@@ -13,10 +20,51 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class IOCTest {
 @Test
     public void test01(){
-        ApplicationContext applicationContext=new AnnotationConfigApplicationContext(MainConfig.class);
+        ApplicationContext applicationContext=new AnnotationConfigApplicationContext(MainConfig2.class);
         String [] names=applicationContext.getBeanDefinitionNames();
         for(String name:names){
             System.out.println(name);
         }
     }
+    @Test
+    public void test2(){
+    ApplicationContext applicationContext=new AnnotationConfigApplicationContext(MainConfig2.class);
+    String[] names=applicationContext.getBeanDefinitionNames();
+    for (String name:names){
+        System.out.println(name);
+    }
+    }
+    @Test
+    public void test3(){
+    ApplicationContext applicationContext=new AnnotationConfigApplicationContext(MainConfig2.class);
+        System.out.println("IOC容器创建完成");
+    Object bean=applicationContext.getBean("sunzhipeng");
+    Object bean2=applicationContext.getBean("sunzhipeng");
+        System.out.println(bean==bean2);
+    }
+    @Test
+    public void test4(){
+    ApplicationContext applicationContext=new AnnotationConfigApplicationContext(MainConfig2.class);
+    String[] names = applicationContext.getBeanNamesForType(Person.class);
+        Environment environment = applicationContext.getEnvironment();
+        //动态获取环境变量的值，win10
+       String property= environment.getProperty("os.name");
+        System.out.println(property);
+       for (String name:names){
+           System.out.println(name);}
+        Map<String, Person> persons = applicationContext.getBeansOfType(Person.class);
+        System.out.println(persons);
+    }
+    AnnotationConfigApplicationContext applicationContext=new AnnotationConfigApplicationContext(MainConfig2.class);
+    private void printBeans(AnnotationConfigApplicationContext applicationContext){
+        String[] definitionNames = applicationContext.getBeanDefinitionNames();
+        for (String name : definitionNames) {
+            System.out.println(name);
+        }
+    }
+    @Test
+    public void testImport(){
+        printBeans(applicationContext);
+    }
+
 }
